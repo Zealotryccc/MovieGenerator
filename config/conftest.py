244818c5c71766931917ee269d657f14d00edee1
Kir <kirillka3007@yandex.ru>
2026-04-28 12:58:37 +0500

    Дописать test_models
    c

--- config/conftest.py
+++ config/conftest.py
@@ -1,23 +1,58 @@
 import pytest
-from movie_generator.models import Author,Movie,Tag
+from movie_generator.models import Country, Genre, Actor, Movie, Review, Tag
 
 @pytest.fixture
-def author(db):
-    return Author.objects.create(
-        name = "Борис Борисыч"
+def country(db):
+    return Country.objects.create(
+        name="США"
     )
+
+@pytest.fixture
+def genre(db):
+    return Genre.objects.create(
+        name="Боевик"
+    )
+
+@pytest.fixture
+def actor(db):
+    return Actor.objects.create(
+        name="Леонардо ДиКаприо",
+        photo="https://example.com/dicaprio.jpg"
+    )
+
 @pytest.fixture
 def tag(db):
     return Tag.objects.create(
-        name = "Фантастика"
+        name="кассовый"
     )
+
 @pytest.fixture
-def movie(author, tag):
+def movie(country, db):
+    # Создаём фильм без ManyToMany полей (genres, actors)
     movie = Movie.objects.create(
-        title="Маленький принц",
-        description="1111",
-        published_at="2007-03-14"
+        title="Начало",
+        description="Фильм о проникновении в сны",
+        poster="https://example.com/inception.jpg",
+        release_date="2010-07-16",
+        country=country,
+        director="Кристофер Нолан",
+        duration=148,
+        age_rating="12+"
     )
-    movie.authors.add(author)
-    movie.tags.add(tag)
     return movie
+
+@pytest.fixture
+def movie_with_genres_and_actors(movie, genre, actor):
+    # Добавляем связи ManyToMany
+    movie.genres.add(genre)
+    movie.actors.add(actor)
+    return movie
+
+@pytest.fixture
+def review(movie, db):
+    return Review.objects.create(
+        movie=movie,
+        author="Иван Петров",
+        rating=9,
+        text="Шедевр! Обязательно к просмотру"
+    )
--- config/movie_generator/tests/test_models.py
+++ config/movie_generator/tests/test_models.py
@@ -1,147 +1,36 @@
 import pytest
-from django.core.exceptions import ValidationError
-from django.db import IntegrityError
-from datetime import date
-from ..models import Country, Genre, Actor, Movie, Review, Tag
-
+from django.urls import reverse
+from django.test import Client
+from movie_generator.models import Movie, Review, Country, Genre, Actor, Tag
 
 @pytest.mark.django_db
-class TestCountry:
-    def test_create_country(self):
-        country = Country.objects.create(name="USA")
-        assert country.name == "USA"
-        assert str(country) == "USA"
-    
-    def test_country_unique_constraint(self):
-        Country.objects.create(name="USA")
-        with pytest.raises(IntegrityError):
-            Country.objects.create(name="USA")
+def test_country(country):
+    assert country.name == "США"
+    assert Country.objects.count() == 1
 
+def test_genre(genre):
+    assert genre.name == "Боевик"
+    assert Genre.objects.count() == 1
 
-@pytest.mark.django_db
-class TestGenre:
-    def test_create_genre(self):
-        genre = Genre.objects.create(name="Comedy")
-        assert genre.name == "Comedy"
-        assert str(genre) == "Comedy"
+def test_actor(actor):
+    assert actor.name == "Леонардо ДиКаприо"
+    assert actor.photo == "https://example.com/dicaprio.jpg"
+    assert Actor.objects.count() == 1 
 
+def test_tag(tag):
+    assert tag.name == "кассовый"
+    assert Tag.objects.count() == 1
 
-@pytest.mark.django_db
-class TestActor:
-    def test_create_actor(self):
-        actor = Actor.objects.create(name="Tom Hanks")
-        assert actor.name == "Tom Hanks"
-        assert str(actor) == "Tom Hanks"
+def test_movie(movie):
+    assert movie.title == "Начало"
+    assert movie.description == "Фильм о проникновении в сны"
+    assert movie.poster == "https://example.com/inception.jpg"
+    assert movie.release_date == "2010-07-16"
+    assert movie.country == Country
+    assert movie.director == "Кристофер Нолан"
+    assert movie.duration == 148
+    assert movie.age_rating == "12+"
 
-
-@pytest.mark.django_db
-class TestMovie:
-    def setup_method(self):
-        self.country = Country.objects.create(name="USA")
-        self.genre = Genre.objects.create(name="Drama")
-        self.actor = Actor.objects.create(name="Tom Hanks")
-    
-    def test_create_movie(self):
-        movie = Movie.objects.create(
-            title="Forrest Gump",
-            description="The story of Forrest Gump",
-            poster="posters/forrest_gump.jpg",
-            release_date=date(1994, 7, 6),
-            country=self.country,
-            director="Robert Zemeckis",
-            duration=142,
-            age_rating="16+"
-        )
-        movie.genres.add(self.genre)
-        movie.actors.add(self.actor)
-        
-        assert movie.title == "Forrest Gump"
-        assert movie.duration == 142
-        assert movie.age_rating == "16+"
-        assert str(movie) == "Forrest Gump"
-    
-    def test_movie_average_rating(self):
-        movie = Movie.objects.create(
-            title="Test Movie",
-            description="Test description",
-            poster="posters/test.jpg",
-            release_date=date(2023, 1, 1),
-            country=self.country,
-            director="Test Director",
-            duration=120,
-            age_rating="12+"
-        )
-        
-        Review.objects.create(movie=movie, author="User1", rating=8, text="Good")
-        Review.objects.create(movie=movie, author="User2", rating=10, text="Excellent")
-        
-        assert movie.average_rating == 9.0
-    
-    def test_movie_average_rating_no_reviews(self):
-        movie = Movie.objects.create(
-            title="Test Movie",
-            description="Test description",
-            poster="posters/test.jpg",
-            release_date=date(2023, 1, 1),
-            country=self.country,
-            director="Test Director",
-            duration=120,
-            age_rating="12+"
-        )
-        
-        assert movie.average_rating == 0
-
-
-@pytest.mark.django_db
-class TestReview:
-    def setup_method(self):
-        self.country = Country.objects.create(name="USA")
-        self.movie = Movie.objects.create(
-            title="Test Movie",
-            description="Test description",
-            poster="posters/test.jpg",
-            release_date=date(2023, 1, 1),
-            country=self.country,
-            director="Test Director",
-            duration=120,
-            age_rating="12+"
-        )
-    
-    def test_create_review(self):
-        review = Review.objects.create(
-            movie=self.movie,
-            author="John Doe",
-            rating=8,
-            text="Great movie!"
-        )
-        
-        assert review.rating == 8
-        assert review.author == "John Doe"
-        assert str(review) == f"{self.movie.title} - John Doe"
-    
-    def test_review_rating_validation(self):
-        with pytest.raises(ValidationError):
-            review = Review(
-                movie=self.movie,
-                author="John Doe",
-                rating=11,
-                text="Great movie!"
-            )
-            review.full_clean()
-        
-        with pytest.raises(ValidationError):
-            review = Review(
-                movie=self.movie,
-                author="John Doe",
-                rating=0,
-                text="Great movie!"
-            )
-            review.full_clean()
-
-
-@pytest.mark.django_db
-class TestTag:
-    def test_create_tag(self):
-        tag = Tag.objects.create(name="Action")
-        assert tag.name == "Action"
-        assert str(tag) == "Action"
+def test_movie_with_genres_and_actors(movie,genre,actor):
+    assert movie.genres.add(genre)
+    movie.actors
